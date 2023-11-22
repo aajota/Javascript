f631641f468772edf0de2647aaeb93f3f61864b7
Antonio Lima Goes Junior <ajlima8@gmail.com>
2023-11-22 06:42:51 -0300

    imagens e css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('QAX, !')
+
+var userName = 'Ajota'
+
+document.getElementById('user-Name').innerHTML = userName
+
+//var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+//console.log(typeof jedi)
