4b1403e222a848665985c728a2696865fcd4bda4
Antonio Lima Goes Junior <ajlima8@gmail.com>
2023-11-24 06:30:59 -0300

    operadores matemáticos

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var userName = 'Ajota'
 
 document.getElementById('user-Name').innerHTML = userName
 
+//variaveis//
+
 //var nome = 'Mestre Yoda'
 //var idade = 100
 //var jedi = true
@@ -11,3 +13,18 @@ document.getElementById('user-Name').innerHTML = userName
 //console.log(typeof nome)
 //console.log(typeof idade)
 //console.log(typeof jedi)
+
+//operadores matematicos//
+
+var n1 = 5
+var n2 = 5
+
+console.log(typeof n1)
+
+// + somar
+// - subitrair
+// * mutiplicar
+// / dividir
+
+var total = n1 + n2
+console.log(total)
