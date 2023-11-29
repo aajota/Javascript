715a6a2b8f6ef0a7e1104c99bc938f4c21e494c5
Antonio Lima Goes Junior <ajlima8@gmail.com>
2023-11-29 06:37:28 -0300

    operadores de comparação

--- js/app.js
+++ js/app.js
@@ -14,17 +14,91 @@ document.getElementById('user-Name').innerHTML = userName
 //console.log(typeof idade)
 //console.log(typeof jedi)
 
+
+
+
+
+
+
 //operadores matematicos//
 
-var n1 = 5
-var n2 = 5
+// var n1 = 5
+// var n2 = 5
 
-console.log(typeof n1)
+// console.log(typeof n1)
 
 // + somar
 // - subitrair
 // * mutiplicar
 // / dividir
 
-var total = n1 + n2
-console.log(total)
+// var total = n1 + n2
+// console.log(total)
+
+
+
+
+
+
+
+
+// operadores de comparação
+
+// var v1 = 5
+// var v2 = 5
+
+// var resultado = v1 === v2
+// console.log(resultado)
+
+// Igual (==)
+// Retorna verdadeiro se os valores comparados forem iguais.
+
+// 1=='1' //true
+
+// Não igual (!=)
+// Retorna verdadeiro se os valores comparados não forem iguais. Esse operador também pode ser chamado de diferente de.
+
+// 4!=1 //true
+
+// Igual estrito (===)
+// Esse operador é mais severo, só retorna verdadeiro se o valor e o tipo comparados forem iguais.
+
+// 3==='3' //false
+// 3===3 //true
+
+// Não igual estrito (!==)
+// Não se engane, esse operador vai retornar verdadeiro se os valores e ou os tipos forem diferentes
+
+// 3!=='3' //true
+// 3!==3 //false
+// 3!==4 //true
+
+// Maior que (>)
+// Compara se o operador da esquerda é maior que o da direita. Caso seja retorna verdadeiro
+
+// 1>2 //false
+// 5>3 //true
+// 4>'1' //true
+
+// Maior ou igual que (>=)
+// Compara se o operador da esquerda é maior ou igual ao da direita. Caso seja retorna verdadeiro
+
+// 1>=2 //false
+// 5>=3 //true
+// 4>='1' //true
+// 2>=2 // true
+
+// Menor que (<)
+// Compara se o operador da esquerda é menor que o da direita. Caso seja retorna verdadeiro
+
+// 1<2 //true
+// 5<3 //false
+// 4<'1' //false    
+
+// Menor ou igual que (<=)
+// Compara se o operador da esquerda é menor ou igual ao da direita. Caso seja retorna verdadeiro
+
+// 1<=2 //true
+// 5<=3 //false
+// 4<='1' //false
+// 2<=2 // true
